678c84ba0ec97a3692871d0926045e6940778b99
dustin norlander <dustinn@gmail.com>
2011-03-16 11:13:41 -0700

    Add toXMLString method

--- src/com/trendrr/oss/DynMap.java
+++ src/com/trendrr/oss/DynMap.java
@@ -10,7 +10,9 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
@@ -277,5 +279,71 @@ public class DynMap extends HashMap<String,Object>{
 			}
 		}
 		return str.toString();
+	}
+	
+	private String toXMLStringCollection(java.util.Collection c) {
+		if (c == null)
+			return "";
+
+		String collection = "";
+		for (Object o : c) {
+			collection += "<item>";
+			if (o instanceof DynMap)
+				collection += ((DynMap) o).toXMLString();
+			else if (o instanceof java.util.Collection) {
+				for (Object b : (java.util.Collection) o) {
+					collection += "<item>";
+					if (b instanceof java.util.Collection)
+						collection += this
+								.toXMLStringCollection((java.util.Collection) b);
+					else
+						collection += b.toString();
+					collection += "</item>";
+				}
+			} else if (o instanceof java.util.Map) {
+				DynMap dm = new DynMap();
+				dm.putAll((java.util.Map) o);
+				collection += dm.toXMLString();
+			} else
+				collection += o.toString();
+			collection += "</item>";
+		}
+		return collection;
+	}
+	
+	
+	/**
+	 * Constructs an xml string from this dynmap.  
+	 * @return
+	 */
+	public String toXMLString() {
+		if (this.isEmpty())
+			return null;
+
+		StringBuilder buf = new StringBuilder();
+		Iterator iter = this.entrySet().iterator();
+		while (iter.hasNext()) {
+			Map.Entry entry = (Map.Entry) iter.next();
+			String element = String.valueOf(entry.getKey());
+			buf.append("<" + element + ">");
+			if (entry.getValue() instanceof DynMap) {
+				buf.append(((DynMap) entry.getValue())
+						.toXMLString());
+			} else if ((entry.getValue()) instanceof java.util.Collection) {
+				buf.append(this
+						.toXMLStringCollection((java.util.Collection) entry
+								.getValue()));
+			} else if ((entry.getValue()) instanceof java.util.Map) {
+				DynMap dm = DynMapFactory.instance(entry.getValue());
+				buf.append(dm.toXMLString());
+			} else if ((entry.getValue()) instanceof Date) {
+				buf.append(IsoDateUtil.getIsoDateNoMillis(((Date)entry.getValue())));
+			} else {
+				buf.append(entry.getValue());
+			}
+			buf.append("</" + element + ">");
+		}
+
+		return buf.toString();
 	}
 }
