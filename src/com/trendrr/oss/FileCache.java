42b588276aa0e1da418bfcb095aed2e3619639a0
dustin norlander <dustinn@gmail.com>
2011-05-04 16:08:56 -0700

    Add max for filecache

--- src/com/trendrr/oss/FileCache.java
+++ src/com/trendrr/oss/FileCache.java
@@ -3,6 +3,7 @@
  */
 package com.trendrr.oss;
 
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.logging.Log;
@@ -25,6 +26,42 @@ public class FileCache {
 	protected Log log = LogFactory.getLog(FileCache.class);
 	
 	private static ConcurrentHashMap<String, ReinitObject<byte[]>> cache = new ConcurrentHashMap<String, ReinitObject<byte[]>> ();
+
+	//absolute max number of items to cache.
+	private int maxItems = 200;
+
+	/**
+	 * max number of items to cache. 
+	 * 
+	 * this is a hard limit.  
+	 * @return
+	 */
+	public int getMaxItems() {
+		return maxItems;
+	}
+
+
+	public void setMaxItems(int maxItems) {
+		this.maxItems = maxItems;
+	}
+
+
+	/**
+	 * gets the file as a utf8 string or null;
+	 * @param filename
+	 * @param timeoutPeriod
+	 * @return
+	 */
+	public String getFileString(String filename, long timeoutPeriod) {
+		byte[] bytes = this.getFileBytes(filename, timeoutPeriod);
+		if (bytes == null)
+			return null;
+		try {
+			return new String(bytes, "UTF8");
+		} catch (UnsupportedEncodingException e) {
+			return null;
+		}
+	}
 	
 	
 	/**
@@ -34,6 +71,13 @@ public class FileCache {
 	 * @return
 	 */
 	public byte[] getFileBytes(String filename, long timeoutPeriod) {
+		if (cache.size() > this.maxItems) {
+			//TODO: this could cause a threadsafety problem below
+			//this should be smarter, but for now this is only a 
+			//catch for a potential memory leak
+			cache.clear();
+		}
+		
 		final String f = filename;
 		
 		//fancy timeout cache. 
