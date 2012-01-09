cc4d6fa0666e3bc6d5035321ae755a1b5967d6a4
dustin norlander <dustinn@gmail.com>
2012-01-09 11:52:11 -0800

    add a wrapper class to attache metadata to any byte array

---
+++ src/com/trendrr/oss/cache/TrendrrCacheItem.java
@@ -0,0 +1,118 @@
+/**
+ * 
+ */
+package com.trendrr.oss.cache;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.trendrr.oss.DynMap;
+import com.trendrr.oss.exceptions.TrendrrParseException;
+
+
+/**
+ * 
+ * Simple class that allows us to store aribitrary dynmap as metadata to the a byte array.
+ * 
+ * @author Dustin Norlander
+ * @created Jan 9, 2012
+ * 
+ */
+public class TrendrrCacheItem {
+
+	protected Log log = LogFactory.getLog(TrendrrCacheItem.class);
+	
+	public static TrendrrCacheItem instance(DynMap metadata, byte[] bytes) {
+		TrendrrCacheItem item = new TrendrrCacheItem();
+		item.setBytes(bytes);
+		item.setMetadata(metadata);
+		return item;
+	}
+	
+	private byte[] bytes = new byte[0];
+	public byte[] getBytes() {
+		return bytes;
+	}
+
+	public void setBytes(byte[] bytes) {
+		this.bytes = bytes;
+	}
+
+	public DynMap getMetadata() {
+		return metadata;
+	}
+
+	public void setMetadata(DynMap metadata) {
+		this.metadata = metadata;
+	}
+
+	DynMap metadata = new DynMap();
+	
+	
+	public byte[] serialize() {
+		try {
+			byte[] meta = metadata.toJSONString().getBytes("utf8");
+			int length = meta.length;
+			byte[] len = intToByteArray(length);
+			
+			return this.concatAll(len, meta, this.bytes);
+		} catch (UnsupportedEncodingException e) {
+			log.warn("Gah, how'd you get an encodeing exception!?", e);
+		}
+		return null;
+	}
+	
+	/**
+	 * will deserialize the byte array
+	 * @param vals
+	 * @throws UnsupportedEncodingException 
+	 */
+	public static TrendrrCacheItem deserialize(byte[] bytes) throws TrendrrParseException {
+		int length = byteArrayToInt(Arrays.copyOfRange(bytes, 0, 4));
+		byte[] metabytes = Arrays.copyOfRange(bytes, 4, 4+length);
+		DynMap metadata;
+		try {
+			metadata = DynMap.instance(new String(metabytes, "utf8"));
+		} catch (UnsupportedEncodingException e) {
+			throw new TrendrrParseException(e);
+		}
+		byte[] b = Arrays.copyOfRange(bytes, 4+length, bytes.length);
+		
+		TrendrrCacheItem item = new TrendrrCacheItem();
+		item.setBytes(b);
+		item.setMetadata(metadata);
+		return item;
+	}
+	
+	protected byte[] concatAll(byte[] first, byte[]... rest) {
+		  int totalLength = first.length;
+		  for (byte[] array : rest) {
+		    totalLength += array.length;
+		  }
+		  byte[] result = Arrays.copyOf(first, totalLength);
+		  int offset = first.length;
+		  for (byte[] array : rest) {
+		    System.arraycopy(array, 0, result, offset, array.length);
+		    offset += array.length;
+		  }
+		  return result;
+	}
+	
+	private static byte[] intToByteArray(int value) {
+        return new byte[] {
+                (byte)(value >>> 24),
+                (byte)(value >>> 16),
+                (byte)(value >>> 8),
+                (byte)value};
+	}
+	
+	private static int byteArrayToInt(byte [] b) {
+	        return (b[0] << 24)
+	                + ((b[1] & 0xFF) << 16)
+	                + ((b[2] & 0xFF) << 8)
+	                + (b[3] & 0xFF);
+	}
+}
---
+++ src/com/trendrr/oss/tests/TrendrrCacheItemTests.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.trendrr.oss.tests;
+
+import java.io.UnsupportedEncodingException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.trendrr.oss.DynMap;
+import com.trendrr.oss.cache.TrendrrCacheItem;
+import com.trendrr.oss.exceptions.TrendrrParseException;
+
+
+/**
+ * @author Dustin Norlander
+ * @created Jan 9, 2012
+ * 
+ */
+public class TrendrrCacheItemTests {
+
+	protected Log log = LogFactory.getLog(TrendrrCacheItemTests.class);
+	
+	@Test
+	public void test1() throws UnsupportedEncodingException, TrendrrParseException {
+		DynMap metadata = new DynMap();
+		metadata.put("key1", "something something");
+		
+		TrendrrCacheItem item = TrendrrCacheItem.instance(metadata, "this is the message".getBytes("utf8"));
+		
+		byte[] bytes = item.serialize();
+		
+		TrendrrCacheItem item2 = TrendrrCacheItem.deserialize(bytes);
+		
+		System.out.println(item2.getMetadata().toJSONString());
+		
+		Assert.assertArrayEquals(bytes, item2.serialize());
+		
+		
+	}
+}
