e39daefb8239fcc9c278b0893787e567403b778a
Dustin Norlander <dustinn@gmail.com>
2012-06-20 14:44:02 -0700

    Add ssl support

--- src/main/com/trendrr/oss/networking/http/Http.java
+++ src/main/com/trendrr/oss/networking/http/Http.java
@@ -5,6 +5,7 @@ package com.trendrr.oss.networking.http;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -15,6 +16,9 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocketFactory;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -42,7 +46,7 @@ public class Http {
 	
 	public static void main(String ...strings) throws Exception {
 		HttpRequest request = new HttpRequest();
-		request.setUrl("http://google.com");
+		request.setUrl("https://google.com");
 		request.setMethod("POST");
 		request.setContent("application/json", "this is something something".getBytes());
 		request(request);
@@ -60,23 +64,58 @@ public class Http {
 			port = TypeCast.cast(Integer.class, tmp[1]);
 		}
 		
-		if (request.isSSL()) {
-			//uhh, what the hell do we do here?
-			
-			
-		} else {
-			try {
-//				Socket s = new Socket(host, port);
-//				
-//				OutputStream os = s.getOutputStream();
-//				os.write(request.toByteArray());
-//				os.flush();
-//				BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-//				String t;
-//				
+		try {
+			if (request.isSSL()) {
+				System.out.println("SSL!");
+				//uhh, what the hell do we do here?
+			   if (port == 80) {
+				   port = 443;
+			   }
+	
+			    SocketFactory socketFactory = SSLSocketFactory.getDefault();
+			    Socket socket = socketFactory.createSocket(host, port);
+	
+			    // Create streams to securely send and receive data to the server
+			    InputStream in = socket.getInputStream();
+			    OutputStream out = socket.getOutputStream();
+			    out.write(request.toByteArray());
+			    
+			    // Read from in and write to out...
+			    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+				String t;
+				
 //				s.getInputStream().r
-//				while((t = br.readLine()) != null) System.out.println(t);
-//				br.close();
+				StringBuilder headerBuilder = new StringBuilder();
+				while(!(t = br.readLine()).isEmpty()) {
+					headerBuilder.append(t).append("\r\n");
+				}
+				String headers = headerBuilder.toString();
+				System.out.println(headers);
+				byte[] content = new byte[getContentLength(headers)];
+				in.read(content);
+				
+				br.close();
+			    
+			    // Close the socket
+			    in.close();
+			    out.close();
+			    HttpResponse response = HttpResponse.parse(headers);
+				response.setContent(content);
+				return response;
+			
+			} else {
+				
+	//				Socket s = new Socket(host, port);
+	//				
+	//				OutputStream os = s.getOutputStream();
+	//				os.write(request.toByteArray());
+	//				os.flush();
+	//				BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+	//				String t;
+	//				
+	//				s.getInputStream().r
+	//				while((t = br.readLine()) != null) System.out.println(t);
+	//				br.close();
 				
 				
 				
@@ -88,25 +127,30 @@ public class Http {
 				wrapper.write(request.toByteArray());
 				
 				String headers = wrapper.readUntil("\r\n\r\n", Charset.forName("utf8"), true);
-				String tmp = Regex.matchFirst(headers, "Content\\-Length\\:\\s+[0-9]+", true);
-				int length = 0;
-				if (tmp != null) {
-					length = TypeCast.cast(Integer.class, tmp.replaceAll("[^0-9]", ""), 0);
-				}
-				byte[] contentbytes = wrapper.readBytes(length);
+				
+				byte[] contentbytes = wrapper.readBytes(getContentLength(headers));
 				wrapper.close();
 				HttpResponse response = HttpResponse.parse(headers);
 				response.setContent(contentbytes);
 				return response;
-//			} catch (TrendrrException x) {
-//				throw x;
-			} catch (IOException x) {
-				throw new TrendrrIOException(x);
+				
 			}
+		} catch (IOException x) {
+			throw new TrendrrIOException(x);
+		} catch (Exception x) {
+			throw new TrendrrException(x);
 		}
-		throw new TrendrrException("Uhh, somethings wrong");
 	}
 	
+	private static int getContentLength(String headers) {
+		String tmp = Regex.matchFirst(headers, "Content\\-Length\\:\\s+[0-9]+", true);
+		int length = 0;
+		if (tmp != null) {
+			length = TypeCast.cast(Integer.class, tmp.replaceAll("[^0-9]", ""), 0);
+		}
+		return length;
+		
+	}
 	public static String get(String url, DynMap params) throws TrendrrNetworkingException {
 		try {
 			if (!url.contains("?")) {
