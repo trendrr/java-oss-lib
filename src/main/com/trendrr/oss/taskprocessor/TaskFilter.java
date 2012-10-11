f63d8dc8f3170b258b87316d0adad872996a04c3
Dustin Norlander <dustinn@gmail.com>
2012-10-11 11:46:49 -0700

    add getName and some additional comments

--- src/main/com/trendrr/oss/taskprocessor/TaskFilter.java
+++ src/main/com/trendrr/oss/taskprocessor/TaskFilter.java
@@ -3,8 +3,12 @@
  */
 package com.trendrr.oss.taskprocessor;
 
+import java.util.Date;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import com.trendrr.oss.executionreport.ExecutionReport;
 
 
 /**
@@ -16,7 +20,30 @@ public abstract class TaskFilter {
 
 	protected static Log log = LogFactory.getLog(TaskFilter.class);
 	
-	
+	/**
+	 * does the processing of this task.  the filter chain will stop if this returns null or throws an exception,
+	 * else the next filter will run with the results from this filter.
+	 * 
+	 * TaskFilters should be stateless, and assumed to be running in multiple threads on multiple tasks. 
+	 * 
+	 * @param task
+	 * @return
+	 * @throws Exception
+	 */
 	public abstract Task doFilter(Task task) throws Exception;
 	
+//	/**
+//	 * increments a key as a child of this filter.
+//	 * @param task
+//	 * @param key
+//	 * @param amount
+//	 * @param start
+//	 */
+//	public void executionReportInc(Task task, String key, long amount, Date start) {
+//		ExecutionReport.instance("TaskProcessor").inc(task.getProcessor().getName() + "." + this.getName() + "." + key, amount, start);
+//	}
+	
+	public String getName() {
+		return this.getClass().getSimpleName();
+	}
 }
