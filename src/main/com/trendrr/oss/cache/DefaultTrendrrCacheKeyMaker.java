b9b5e2ec6a60e2ea8623bb98c945fa9095431d7a
Dustin Norlander <dustinn@gmail.com>
2012-10-16 14:06:53 -0700

    better keymaker.  hash if any invalid chars are present in the key

--- src/main/com/trendrr/oss/cache/DefaultTrendrrCacheKeyMaker.java
+++ src/main/com/trendrr/oss/cache/DefaultTrendrrCacheKeyMaker.java
@@ -20,7 +20,13 @@ public class DefaultTrendrrCacheKeyMaker implements TrendrrCacheKeyMaker {
 
 	protected static Log log = LogFactory
 			.getLog(DefaultTrendrrCacheKeyMaker.class);
-
+	
+	/**
+	 * we sha1 hash any keys longer then this.
+	 */
+	public static int MAX_KEY_LENGTH_BEFORE_HASH = 24;
+	
+	
 	/* (non-Javadoc)
 	 * @see com.trendrr.oss.cache.TrendrrCacheKeyMaker#toKey(java.lang.String, java.lang.String)
 	 */
@@ -29,14 +35,33 @@ public class DefaultTrendrrCacheKeyMaker implements TrendrrCacheKeyMaker {
 		if (namespace != null) {
 			key = namespace + key;
 		}
-		if (key.length() > 24) {
+		if (shouldHashKey(key)) {
 			try {
 				key = StringHelper.sha1Hex(key.getBytes("utf8"));
 			} catch (UnsupportedEncodingException e) {
 				log.warn("Invalid key: " + key, e);
 			}
 		} 
-//		log.info("key: " + key);
 		return key;
+	}
+	
+	
+	protected static boolean shouldHashKey(String key) {
+		try {
+			byte[] keyBytes = key.getBytes("utf8");
+			// Validate the key
+			if (key.length()> MAX_KEY_LENGTH_BEFORE_HASH) {
+				return true;
+			}
+			for (byte b : keyBytes) {
+		      if (b == ' ' || b == '\n' || b == '\r' || b == 0) {
+		    	  return true;
+		      }
+		    }
+			return false;
+		} catch (UnsupportedEncodingException e) {
+			log.error("Caught", e);
+		}
+		return true;   
 	}
 }
