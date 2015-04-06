2371fddaedcb996d56474319cac856f412141ef9
Mark Grabois <mgrabois@twitter.com>
2015-04-06 18:27:35 -0400

    doc

--- src/main/com/trendrr/oss/DynMap.java
+++ src/main/com/trendrr/oss/DynMap.java
@@ -114,7 +114,7 @@ public class DynMap extends HashMap<String,Object> implements JSONAware{
 	 * puts the value if the key is absent (or null).
 	 * @param key
 	 * @param val
-	 * @return the val for the key if it is not null, else returns null (as in java 8 java.util.HashMap)
+	 * @return null if the key is absent, else returns the existing val for that key (as in java 8 java.util.HashMap)
 	 */
 	public Object putIfAbsent(String key, Object val) {
 		if (this.get(key) == null) {
